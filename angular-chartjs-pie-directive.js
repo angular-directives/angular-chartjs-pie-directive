791941772142e09d13ca70c9e546a0f46dfce786
Stéphane Bégaudeau <stephane.begaudeau@gmail.com>
2013-06-09 21:42:36 +0200

    Add the initial version of the directive.

---
+++ angular-chartjs-pie-directive.js
@@ -0,0 +1,73 @@
+/*!
+ * AngularJS Chart.js Pie Directive
+ *
+ * Copyright 2013 Stephane Begaudeau
+ * Released under the MIT license
+ */
+angular.module('angular.directives-chartjs-pie', []).directive('angChartjsPie', [function () {
+  var compilationFunction = function (templateElement, templateAttributes, transclude) {
+    if (templateElement.length === 1) {
+      var node = templateElement[0];
+
+      var width = node.getAttribute('data-chartjs-width') || '400';
+      var height = node.getAttribute('data-chartjs-height') || '400';
+
+      var canvas = document.createElement('canvas');
+      canvas.setAttribute('width', width);
+      canvas.setAttribute('height', height);
+      canvas.setAttribute('data-chartjs-model', node.getAttribute('data-chartjs-model'));
+
+      var options = {};
+
+      var potentialOptions = [
+        {key:'data-chartjs-segment-show-stroke', value:'segmentShowStroke', isBoolean: true},
+        {key:'data-chartjs-segment-stroke-color', value:'segmentStrokeColor'},
+        {key:'data-chartjs-segment-stroke-width', value:'segmentStrokeWidth', isNumber: true},
+        {key:'data-chartjs-animation', value:'animation', isBoolean: true},
+        {key:'data-chartjs-animation-steps', value:'animationSteps', isNumber: true},
+        {key:'data-chartjs-animation-easing', value:'animationEasing'},
+        {key:'data-chartjs-animation-rotate', value:'animationRotate', isBoolean: true},
+        {key:'data-chartjs-animation-scale', value:'animationScale', isBoolean: true}
+      ];
+
+      for (var i = 0; i < potentialOptions.length; i++) {
+        var aKey = node.getAttribute(potentialOptions[i].key);
+        if (aKey && potentialOptions[i].isBoolean) {
+          if ('true' === aKey) {
+            options[potentialOptions[i].value] = true;
+          } else if ('false' === aKey) {
+            options[potentialOptions[i].value] = false;
+          }
+        } else if (aKey && potentialOptions[i].isNumber) {
+          options[potentialOptions[i].value] = parseInt(aKey);
+        }else if (aKey) {
+          options[potentialOptions[i].value] = aKey;
+        }
+      }
+
+      var chart = new Chart(canvas.getContext('2d'));
+      node.parentNode.replaceChild(canvas, node);
+
+      return {
+        pre: function preLink(scope, instanceElement, instanceAttributes, controller) {
+          var expression = canvas.getAttribute('data-chartjs-model');
+          scope.$watch(expression, function (newValue, oldValue) {
+            var callback = scope[node.getAttribute('data-chartjs-on-animation-complete')];
+            if (callback !== undefined) {
+              options.onAnimationComplete = callback;
+            }
+
+            chart.Pie(newValue, options);
+          }, true);
+        },
+        post: function postLink(scope, instanceElement, instanceAttributes, controller) {}
+      };
+    }
+  };
+
+  var chartjsBar = {
+    compile: compilationFunction,
+    replace: true
+  };
+  return chartjsBar;
+}]);
